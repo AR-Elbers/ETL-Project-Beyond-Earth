ad67008c330e4d7320940cf7a4515e16e2d8d445
unknown <gail.chugg@bigpond.com>
2020-11-28 14:16:13 +0800

    update

--- ETL Project Mars.sql
+++ ETL Project Mars.sql
@@ -2,25 +2,25 @@ DROP TABLE facts
 ;
  
  CREATE TABLE facts (
-	Planet VARCHAR Primary Key,  
-  	Mass decimal (18,4)   NOT NULL,
-     Diameter INT   NOT NULL,
-     Density INT   NOT NULL,
-     Gravity decimal(18,1)   NOT NULL,
-     Escape_Velocity decimal (18,1)   NOT NULL,
- 	 Rotation_Period decimal (18,1) NOT NULL,
-     Length_of_Day decimal(18,1) NOT NULL,
-     Distance_from_Sun decimal(18,1) NOT NULL,
-     Perihelion decimal(18,1) NOT NULL,
-     Aphelion decimal(18,1) NOT NULL,
-     Orbital_Period decimal(18,1) NOT NULL,
-     Orbital_Velocity decimal(18,1)   NOT NULL,
-     Orbital_Inclination decimal(18,1)   NOT NULL,
-     Orbital_Eccentricity decimal(18,3)   NOT NULL,
-     Obliquity_to_Orbit decimal(18,3)  NOT NULL,
-     Mean_Temperature INT   NOT NULL,
-     Number_of_Moons INT   NOT NULL,
-     "Ring_System?" BOOLEAN NOT NULL
+	"Planet" VARCHAR Primary Key,  
+  	"Mass" decimal (18,4)   NOT NULL,
+     "Diameter" INT   NOT NULL,
+     "Density" INT   NOT NULL,
+     "Gravity" decimal(18,1)   NOT NULL,
+     "Escape Velocity" decimal (18,1)   NOT NULL,
+ 	 "Rotation Period" decimal (18,1) NOT NULL,
+     "Length of Day" decimal(18,1) NOT NULL,
+     "Distance from Sun" decimal(18,1) NOT NULL,
+     "Perihelion" decimal(18,1) NOT NULL,
+     "Aphelion" decimal(18,1) NOT NULL,
+     "Orbital Period" decimal(18,1) NOT NULL,
+     "Orbital Velocity" decimal(18,1)   NOT NULL,
+     "Orbital Inclination" decimal(18,1)   NOT NULL,
+     "Orbital Eccentricity" decimal(18,3)   NOT NULL,
+     "Obliquity to Orbit" decimal(18,3)  NOT NULL,
+     "Mean Temperature" INT   NOT NULL,
+     "Number of Moons" INT   NOT NULL,
+     "Ring System?" BOOLEAN NOT NULL
  );
 
 SELECT * FROM facts
